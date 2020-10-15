1af0323062999bac6f5454e0b7684bcfed7e999e
tenytskyalex <sanycas@gmail.com>
2020-10-15 18:54:36 +0300

    deleted extra files

--- src/test/java/com/netcracker/eductr/tasks/tests/model/BaseObject.java
+++
@@ -1,9 +0,0 @@
-package com.netcracker.eductr.tasks.tests.model;
-
-public abstract class BaseObject {
-    protected Object instance;
-
-    public Object getInstance() {
-        return instance;
-    }
-}
